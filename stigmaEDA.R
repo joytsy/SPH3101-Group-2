04bb642d451640128f6080360777fe741a7a7507
joytsy <e0774721@u.nus.edu>
2024-10-05 13:57:20 +0800

    subgroup analysis for case_status

--- stigmaEDA.R
+++ stigmaEDA.R
@@ -83,7 +83,7 @@ ggplot(datatb2, aes(x = stigma_threshold)) +
   theme_minimal()
 
 # -------- Data Processing and Manipulation --------
-# Convert the labelled variable to numeric or factor
+# Convert the labelled variable for status during follow-up to numeric
 datatb2 <- datatb2 %>%
   mutate(a1_status_tb_during_fu = as.numeric(a1_status_tb_during_fu))
 
@@ -236,3 +236,57 @@ ggplot(datatb1, aes(x = CES_D_10_threshold)) +
   theme_minimal()
 
 
+##############################################################################
+## Subgroup Analysis (Case status; Smoking status; Cough) on stigma_scores) ##
+##############################################################################
+######### Obtain subgroup datasets from datatb2 ######### 
+
+##### case status
+# create new column & copy over case_status for those who followed-up in datatb2_followup from datatb1
+datatb2_followup <- datatb2_followup %>%
+  mutate(case_status_copy = datatb1$case_status[datatb1$a1_record_id %in% datatb2_followup$a1_record_id])
+# get datasets
+datatb2_followup_case_completed <- datatb2_followup %>%
+  filter(datatb2_followup$case_status_copy == "completed")
+datatb2_followup_case_cured <- datatb2_followup %>%
+  filter(datatb2_followup$case_status_copy == "cured")
+datatb1_followup_case_completed <- datatb1 %>%
+  filter(datatb1$a1_record_id %in% datatb2_followup_case_completed$a1_record_id)
+datatb1_followup_case_cured <- datatb1 %>%
+  filter(datatb1$a1_record_id %in% datatb2_followup_case_cured$a1_record_id)
+
+# Shapiro-Wilk test to test for normality
+# baseline: p-value < 0.01 --> data is likely not normally distributed
+shapiro.test(datatb1_followup_case_completed$stigma_score)
+shapiro.test(datatb1_followup_case_cured$stigma_score)
+shapiro.test(datatb2_followup_case_completed$stigma_score)
+shapiro.test(datatb2_followup_case_cured$stigma_score)
+# all not normally distributed -> wilcox test
+
+##### smoking status
+# create new column & copy over smoking_status a1_q16 for those who followed-up in datatb2_followup from datatb1
+datatb2_followup <- datatb2_followup %>%
+  mutate(smoke_status_copy = datatb1$a1_q16[datatb1$a1_record_id %in% datatb2_followup$a1_record_id])
+# get datasets
+datatb2_followup_smoke <- datatb2_followup %>%
+  filter(datatb2_followup$smoke_status_copy == "completed")
+
+##### cough symptom
+# create new column & copy over cough a1_q28___1 for those who followed-up in datatb2_followup from datatb1
+datatb2_followup <- datatb2_followup %>%
+  mutate(cough_status_copy = datatb1$a1_q28___1[datatb1$a1_record_id %in% datatb2_followup$a1_record_id])
+# get datasets
+
+
+######### Tests ######### 
+##### case status
+wilcox.test(datatb1_followup_case_completed$stigma_score, datatb2_followup_case_completed$stigma_score) #p-val < 0.01
+wilcox.test(datatb1_followup_case_cured$stigma_score, datatb2_followup_case_cured$stigma_score) #p-val < 0.01
+summary(datatb1_followup_case_completed$stigma_score) #mean 15
+summary(datatb2_followup_case_completed$stigma_score) #mean=12.63
+summary(datatb1_followup_case_cured$stigma_score) #mean=16.11
+summary(datatb2_followup_case_cured$stigma_score) #mean=14.36
+
+##### smoking status
+
+##### cough symptom
